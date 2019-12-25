7c7d917b9aaadb6857e3352060df7d6da287d027
KonstantinKuznetsov.kh <45022667+konstantinkuznetsovkh@users.noreply.github.com>
2019-12-25 18:26:19 +0200

    Update script_tutorial.js

--- js/script_tutorial.js
+++ js/script_tutorial.js
@@ -190,7 +190,7 @@ more.addEventListener('click', () => {
 //search and load можно обЪединить 
 function search(target) {
     gapi.client.init({
-        'apiKey': 'AIzaSyDr6mhn4HuvdG2ls17KZPsAjnpSzzqN0mk',
+        'apiKey': 'AIzaSyCFMU84D42hDbBpNMdpnGVMq7Pjeruj-1I',
         'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
     }).then(() => {
         return gapi.client.youtube.search.list({
